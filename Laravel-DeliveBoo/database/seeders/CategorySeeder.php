41ee5173c7fe845cda8a60b64c97e352b4b89874
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-09-10 11:27:13 +0200

    feat: add category seeder

---
+++ Laravel-DeliveBoo/database/seeders/CategorySeeder.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // disabilito relazioni
+        Schema::disableForeignKeyConstraints();
+
+        // truncate
+        Category::truncate();
+
+        // categorie di ristoranti
+        $categories = [
+            'Pizza',
+            'Burgers',
+            'Sushi',
+            'Indian',
+            'Chinese',
+            'Thai',
+            'Kebab',
+            'Fried Chicken',
+            'Pasta',
+            'Vegetarian',
+            'Desserts',
+            'Salads',
+            'Mexican',
+            'Turkish',
+            'Greek',
+            'Healthy',
+            'American',
+            'Japanese',
+            'Italian',
+            'Ice Cream',
+
+        ];
+
+        // numero campi
+        $numRecords = 20;
+
+        // ciclo per popolare tabella categories
+        for ($i = 0; $i < $numRecords; $i++) {
+            $new_category = new Category();
+
+            $new_category->name = $categories[$i];
+            $new_category->slug = Str::of($new_category->name)->slug();
+
+            $new_category->save();
+        }
+
+        // riabilito relazioni
+        Schema::enableForeignKeyConstraints();
+    }
+}
