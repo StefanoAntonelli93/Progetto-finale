1c88259434318d3534e6494399693c8a26834707
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-09-10 15:36:05 +0200

    feat: modified dashboard text

--- Laravel-DeliveBoo/resources/views/dashboard.blade.php
+++ Laravel-DeliveBoo/resources/views/dashboard.blade.php
@@ -13,8 +13,8 @@
                                 {{ session('status') }}
                             </div>
                         @endif
-
-                        {{ __('You are logged in!') }}
+                        {{ ucfirst(Auth::user()->name) }}
+                        {{ __('sei autenticato!') }}
                     </div>
                 </div>
             </div>
