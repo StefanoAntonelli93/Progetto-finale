b3b1b8a2a854dfdfba42b29c8f1ed8d738878fb5
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-09-10 11:04:37 +0200

    feat: fix seeder restaurants

--- Laravel-DeliveBoo/database/seeders/RestaurantSeeder.php
+++ Laravel-DeliveBoo/database/seeders/RestaurantSeeder.php
@@ -51,9 +51,9 @@ class RestaurantSeeder extends Seeder
 
         ];
         // img restaurant
-        $images = ['https://pixabay.com/photos/cafe-tables-and-chairs-bistro-5579069/', 'https://unsplash.com/it/foto/ristorante-vuoto-Yx-lrVk1ZHY', 'https://unsplash.com/it/foto/sgabelli-e-tavolo-da-bar-in-legno-marrone-nx05CDCrfzM', 'https://unsplash.com/it/foto/un-tablet-e-tazze-di-caffe-su-un-tavolo-p7UtCeog33Q', 'https://unsplash.com/it/foto/una-persona-seduta-a-un-tavolo-con-un-piatto-di-cibo-6vfYbDwOuMo'];
+        $images = ['https://pixabay.com/photos/cafe-tables-and-chairs-bistro-5579069/', 'https://unsplash.com/it/foto/ristorante-vuoto-Yx-lrVk1ZHY', 'https://unsplash.com/it/foto/sgabelli-e-tavolo-da-bar-in-legno-marrone-nx05CDCrfzM', 'https://unsplash.com/it/foto/un-tablet-e-tazze-di-caffe-su-un-tavolo-p7UtCeog33Q', 'https://unsplash.com/it/foto/una-persona-seduta-a-un-tavolo-con-un-piatto-di-cibo-6vfYbDwOuMo', 'https://unsplash.com/it/foto/una-persona-seduta-a-un-tavolo-con-un-piatto-di-cibo-6vfYbDwOuMo', 'https://unsplash.com/it/foto/una-persona-seduta-a-un-tavolo-con-un-piatto-di-cibo-6vfYbDwOuMo', 'https://unsplash.com/it/foto/una-persona-seduta-a-un-tavolo-con-un-piatto-di-cibo-6vfYbDwOuMo', 'https://unsplash.com/it/foto/una-persona-seduta-a-un-tavolo-con-un-piatto-di-cibo-6vfYbDwOuMo', 'https://unsplash.com/it/foto/una-persona-seduta-a-un-tavolo-con-un-piatto-di-cibo-6vfYbDwOuMo'];
         // description
-        $descriptions = ['Ambiente accogliente con piatti italiani tradizionali, ingredienti freschi e vini locali.', 'Cucina moderna con vista sul giardino, atmosfera elegante e tranquilla.', 'Specialità di carne alla griglia in un contesto rustico e familiare.', 'Piccolo ristorante con tapas creative e una selezione di vini regionali.', 'Perfetto per brunch e dessert, con ampie vetrate che danno sulla città.'];
+        $descriptions = ['Ambiente accogliente con piatti italiani tradizionali, ingredienti freschi e vini locali.', 'Cucina moderna con vista sul giardino, atmosfera elegante e tranquilla.', 'Specialità di carne alla griglia in un contesto rustico e familiare.', 'Piccolo ristorante con tapas creative e una selezione di vini regionali.', 'Perfetto per brunch e dessert, con ampie vetrate che danno sulla città.', 'Perfetto per brunch e dessert, con ampie vetrate che danno sulla città.', 'Perfetto per brunch e dessert, con ampie vetrate che danno sulla città.', 'Perfetto per brunch e dessert, con ampie vetrate che danno sulla città.', 'Perfetto per brunch e dessert, con ampie vetrate che danno sulla città.', 'Perfetto per brunch e dessert, con ampie vetrate che danno sulla città.'];
         // address
         $addresses = [
             'Via Roma',
@@ -88,12 +88,12 @@ class RestaurantSeeder extends Seeder
 
         for ($i = 0; $i < $numRecords; $i++) {
             $new_restaurant = new Restaurant();
-            $new_restaurant->phone_number = $phone_numbers[$i % count($phone_numbers)];
-            $new_restaurant->restaurant_name = $restaurant_names[$i % count($restaurant_names)];
-            $new_restaurant->img = $images[$i % count($images)];
-            $new_restaurant->description = $descriptions[$i % count($descriptions)];
-            $new_restaurant->address = $addresses[$i % count($addresses)];
-            $new_restaurant->p_iva = $p_iva[$i % count($p_iva)];
+            $new_restaurant->phone_number = $phone_numbers[$i];
+            $new_restaurant->restaurant_name = $restaurant_names[$i];
+            $new_restaurant->img = $images[$i];
+            $new_restaurant->description = $descriptions[$i];
+            $new_restaurant->address = $addresses[$i];
+            $new_restaurant->p_iva = $p_iva[$i];
             $new_restaurant->save();
         }
 
