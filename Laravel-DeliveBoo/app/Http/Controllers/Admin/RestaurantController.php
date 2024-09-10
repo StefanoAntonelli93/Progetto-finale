5d5570cbc2b630590b5a733a01dabafc1473b350
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-09-10 17:51:20 +0200

    feat: add create.blade.php and added data to store -> RestaurantController

--- Laravel-DeliveBoo/app/Http/Controllers/Admin/RestaurantController.php
+++ Laravel-DeliveBoo/app/Http/Controllers/Admin/RestaurantController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Restaurant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RestaurantController extends Controller
 {
@@ -33,7 +34,23 @@ class RestaurantController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // $data = $request->validate();
+        $data = $request->all();
+
+        // visualizzo utente corrente
+        $current_user = Auth::user()->id;
+
+        $restaurant = new Restaurant();
+        $restaurant->restaurant_name = $data['restaurant_name'];
+        $restaurant->description = $data['description'];
+        $restaurant->phone_number = $data['phone_number'];
+        $restaurant->p_iva = $data['p_iva'];
+        $restaurant->address = $data['address'];
+        // visualizzo utente corrente
+        $restaurant->user_id = $current_user;
+        $restaurant->save();
+
+        return redirect()->route('admin.restaurants.index', $restaurant)->with('message', 'Ristorante aggiunto con successo!');
     }
 
     /**
