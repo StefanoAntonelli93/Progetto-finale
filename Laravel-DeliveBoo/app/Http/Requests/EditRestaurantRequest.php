993e42a41a9ec6b8ee84331b49ce02d31870ee6a
MykolaDekusar <ndekusar97@gmail.com>
2024-09-11 12:33:39 +0200

    feat: Created EditRestaurantRequest

---
+++ Laravel-DeliveBoo/app/Http/Requests/EditRestaurantRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class EditRestaurantRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'restaurant_name' => ['required', 'string', 'min:3', 'max:100', Rule::unique('restaurants')->ignore($this->restaurant)],
+            'description' => 'nullable|string|max:2000',
+            'img' => 'nullable|image|max:4096', // massimo 4MB per l'immagine
+            'phone_number' => ['required', 'regex:/^[\+0-9\s\-]+$/', 'min:6', 'max:15', Rule::unique('restaurants')->ignore($this->restaurant)],
+            'address' => 'required|string|max:150',
+            'p_iva' => ['required', 'digits:11', Rule::unique('restaurants')->ignore($this->restaurant)] // Usa digits invece di size per convalidare i numeri
+        ];
+    }
+}
