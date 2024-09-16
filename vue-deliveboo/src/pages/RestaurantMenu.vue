532bf7a90bec71f73b72cf45022f0742b483eae1
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-09-16 13:40:10 +0200

    feat: add layout restaurant menu

--- vue-deliveboo/src/pages/RestaurantMenu.vue
+++ vue-deliveboo/src/pages/RestaurantMenu.vue
@@ -6,10 +6,27 @@ export default {
   components: {
     TitlePage,
   },
+  props: ["restaurantId"],
 };
 </script>
 
 <template>
   <!-- title -->
   <TitlePage :titlePage="'Menu ristorante'"></TitlePage>
+  <div class="container">
+    <div class="content bg-primary"><h1>card ristorante</h1></div>
+
+    <div class="row">
+      <div class="col-8">
+        <div class="content bg-success vh-100">
+          <h2>menu</h2>
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="content bg-warning vh-100">
+          <h1>carrello</h1>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
