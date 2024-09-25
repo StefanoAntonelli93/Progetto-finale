7c871221d020411db1a75f644924256aeea2bb5b
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-09-25 16:32:07 +0200

    feat: registrer blade message password

--- Laravel-DeliveBoo/resources/views/auth/register.blade.php
+++ Laravel-DeliveBoo/resources/views/auth/register.blade.php
@@ -86,7 +86,7 @@
 
                                 <div class="col-md-6">
                                     <input id="password" type="password"
-                                        class="form-control @error('password *') is-invalid @enderror" name="password"
+                                        class="form-control @error('password') is-invalid @enderror" name="password"
                                         required autocomplete="new-password">
 
                                     @error('password')
@@ -99,11 +99,18 @@
 
                             <div class="mb-4 row">
                                 <label for="password-confirm"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Conferma Password *') }}</label>
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Conferma Password') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control"
+                                    <input id="password-confirm" type="password"
+                                        class="form-control @error('password_confirmation') is-invalid @enderror"
                                         name="password_confirmation" required autocomplete="new-password">
+
+                                    @error('password_confirmation')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
                                 </div>
                             </div>
 
